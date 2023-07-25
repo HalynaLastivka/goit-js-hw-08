aa607be88d74157e38edba8a8b5934d7909b2973
HalynaLastivka <lastivkagalina@gmail.com>
2023-07-25 17:37:30 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,30 +1,30 @@
-// // Add imports above this line
-// import { galleryItems } from './gallery-items';
-// // Change code below this line
-console.log('ggggh');
-// import SimpleLightbox from 'simplelightbox';
-// // Додатковий імпорт стилів
-// import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 
-// const ulEl = document.querySelector('.gallery');
-// const newElImg = createLi(galleryItems);
+const ulEl = document.querySelector('.gallery');
+const newElImg = createLi(galleryItems);
 
-// ulEl.insertAdjacentHTML('beforeend', newElImg);
+ulEl.insertAdjacentHTML('beforeend', newElImg);
 
-// // SimpleLightbox
-// const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionPosition: 'bottom', captionsData: 'alt', });
+// SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionPosition: 'bottom', captionsData: 'alt', });
 
 
-// function createLi(gallery) {
-//   return gallery
-//     .map(({ preview, original, description }) => {
-//       return `<li class="gallery__item">
-//         <a class="gallery__link" href="${original}">
-//           <img class="gallery__image" src="${preview}" alt="${description}" />
-//         </a>
-//       </li>`;
-//     })
-//     .join('');
-// }
+function createLi(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>`;
+    })
+    .join('');
+}
 
