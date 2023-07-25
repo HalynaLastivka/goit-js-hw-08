3797c08c5c1ac65484d2db01c61797c62d0a0ed1
HalynaLastivka <lastivkagalina@gmail.com>
2023-07-25 17:34:13 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,30 +1,30 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
-import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
-
-
-const ulEl = document.querySelector('.gallery');
-const newElImg = createLi(galleryItems);
-
-ulEl.insertAdjacentHTML('beforeend', newElImg);
-
-// SimpleLightbox
-const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionPosition: 'bottom', captionsData: 'alt', });
+// // Add imports above this line
+// import { galleryItems } from './gallery-items';
+// // Change code below this line
+console.log('ggggh');
+// import SimpleLightbox from 'simplelightbox';
+// // Додатковий імпорт стилів
+// import "simplelightbox/dist/simple-lightbox.min.css";
 
 
-function createLi(gallery) {
-  return gallery
-    .map(({ preview, original, description }) => {
-      return `<li class="gallery__item">
-        <a class="gallery__link" href="${original}">
-          <img class="gallery__image" src="${preview}" alt="${description}" />
-        </a>
-      </li>`;
-    })
-    .join('');
-}
+// const ulEl = document.querySelector('.gallery');
+// const newElImg = createLi(galleryItems);
+
+// ulEl.insertAdjacentHTML('beforeend', newElImg);
+
+// // SimpleLightbox
+// const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionPosition: 'bottom', captionsData: 'alt', });
+
+
+// function createLi(gallery) {
+//   return gallery
+//     .map(({ preview, original, description }) => {
+//       return `<li class="gallery__item">
+//         <a class="gallery__link" href="${original}">
+//           <img class="gallery__image" src="${preview}" alt="${description}" />
+//         </a>
+//       </li>`;
+//     })
+//     .join('');
+// }
 
